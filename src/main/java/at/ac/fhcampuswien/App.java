37c98ceb5f8fc78a795533c04ee5646ec3037406
Enkhjin <enkhjin.bayarkhuu@stud.fh-campuswien.ac.at>
2022-09-30 15:08:01 +0200

    Task 2 and 3 solved

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,5 +1,8 @@
 package at.ac.fhcampuswien;
 
+import org.graalvm.compiler.phases.graph.ScheduledNodeIterator;
+
+import javax.swing.*;
 import java.util.Scanner;
 
 public class App {
@@ -13,26 +16,87 @@ public class App {
     //todo Task 2
     public void helloRobot(){
         // input your solution here
+        System.out.println("0123456789012345678901");
+        System.out.println("         __");
+        System.out.println(" _(\\    |@@|");
+        System.out.println("(__/\\__ \\--/ __");
+        System.out.println("   \\___|----|  |   __");
+        System.out.println("       \\ }{ /\\ )_ / _\\");
+        System.out.println("       /\\__/\\ \\__O (__");
+        System.out.println("      (--/\\--)    \\__/");
+        System.out.println("      _)(  )(_");
+        System.out.println("     `---''---`");
+
     }
 
     //todo Task 3
     public void sumOfLiterals(){
         // input your solution here
+
+                char z = 'Z';
+                int int_z = z;
+
+                String face = "0xface";
+                int int_face = Integer.decode(face);
+
+                int octal = 012;
+
+                Long long_wert = 80L;
+                int int_long = Math.toIntExact(long_wert);
+
+                double double_wert = 44e-1f;
+                int int_double =(int) double_wert;
+
+                float float_wert = 5.5f;
+                int int_float = (int) float_wert;
+
+                double double_wert8 = 8.88e1;
+                int int_double8 = (int) double_wert8;
+
+                double double_wert9 = 99.9;
+                int int_double9 = (int) double_wert9;
+
+                int sum = int_z + int_face + octal + int_long + int_double +  int_float + int_double8 + int_double9;
+
+
+                System.out.println(sum);
+
+
+                // Hilfe von: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+
+
+
+
+
     }
 
     //todo Task 4
     public void addTwoNumbers(){
         // input your solution here
+      //  Scanner scanner = new Scanner(System.in);
+      //  int number1 = scanner.nextInt;
+       // int number2 = number1;
+       // System.out.println(number1 + number2);
+
     }
 
     //todo Task 5
     public void swapTwoNumbers(){
         // input your solution here
+        int x = 9;
+        int y = 4;
+
+        int tmp = x;
+        tmp = x;
+        x = y;
+        y = tmp;
+
     }
 
     //todo Task 6
     public void compareTwoNumbers(){
         // input your solution here
+
     }
 
     //todo Task 7
